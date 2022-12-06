cc25ae7292497bb0b40486b70e85d15da7c418d2
Overlander <anuragnandal@hotmail.com>
2022-12-05 20:42:58 -0500

    Add state for score and display score

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 
 const Game = () => {
+  const [score, setScore] = useState(0);
 
+  return (
+    <div className="Game">
+      <div>Score: {score}</div>
+    </div>
+  )
 }
 
 export default Game;
