da8ff319a55a299c62870437188e272842b4534a
Overlander <anuragnandal@hotmail.com>
2022-12-06 07:59:10 -0500

    Display 12 cards using Deck of Cards API

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,11 @@
 import React, { useState, useEffect } from "react";
 
-const Card = () => {
-  const [imgSrc, setImgSrc] = useState("");
-  let cardUrl;
-  let cardImage;
-
-  async function getCardUrl() {
-    const deckResponse =  await fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-    const deckData = await deckResponse.json();
-    const deckId = await deckData.deck_id;
-    const cardResponse = await fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
-    const cardData = await cardResponse.json();
-    cardUrl = await cardData.cards[0].image;
-    cardImage = <img src={imgSrc} alt="card"></img>;
-  }
-
-  useEffect(() => {
-    getCardUrl()
-    .then(url => setImgSrc(`https://www.deckofcardsapi.com/api/deck/${url}/draw/?count=1`))
-  }, []);
+const Card = (props) => {
+  const image = <img src={props.url} alt="card"></img>
 
   return(
     <div>
-      {getCardUrl()}
-      <div>{cardImage}</div>
+      {image}
     </div>
   )
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,30 @@
 import React, { useState } from "react";
+import Card from "./Card";
 
 const Game = () => {
   const [score, setScore] = useState(0);
+  // const deckId = "uqa92yhxjgsh";
+  const imageUrls = [
+    "https://deckofcardsapi.com/static/img/8D.png",
+    "https://deckofcardsapi.com/static/img/4S.png",
+    "https://deckofcardsapi.com/static/img/6C.png",
+    "https://deckofcardsapi.com/static/img/QH.png",
+    "https://deckofcardsapi.com/static/img/5D.png",
+    "https://deckofcardsapi.com/static/img/3S.png",
+    "https://deckofcardsapi.com/static/img/0D.png",
+    "https://deckofcardsapi.com/static/img/2D.png",
+    "https://deckofcardsapi.com/static/img/0C.png",
+    "https://deckofcardsapi.com/static/img/4D.png",
+    "https://deckofcardsapi.com/static/img/5C.png",
+    "https://deckofcardsapi.com/static/img/JC.png"
+  ];
 
   return (
     <div className="Game">
       <div>Score: {score}</div>
+      <div className="CardGrid">
+        {imageUrls.map(url => <Card url={url} />)}
+      </div>
     </div>
   )
 }
