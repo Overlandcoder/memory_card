79af39a01e139976c9adafda54f40b10fd99e82a
Overlander <anuragnandal@hotmail.com>
2022-12-07 07:14:23 -0500

    Refactor code

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 const Card = (props) => {
-  const image = <img src={props.url} alt="a card from a deck of cards" className="Card"></img>
+  const { url } = props;
 
-  return(
-    <div>
-      {image}
-    </div>
-  )
+  const image = <img
+                  src={url}
+                  alt="a card from a deck of cards"
+                  className="Card"
+                >
+                </img>;
+
+  return(image);
 }
 
 export default Card;
